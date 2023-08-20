388979e56cad444dee0c56f47cd56e3b1d816aec
varelen <varelen@proton.me>
2023-08-20 16:23:21 +0200

    feat(Service): Initialize resolvers and use config values from nerve options

--- src/Service/Extensions/NerveServiceCollectionExtensions.cs
+++ src/Service/Extensions/NerveServiceCollectionExtensions.cs
@@ -4,8 +4,11 @@
 
 using System.Net;
 
+using Microsoft.Extensions.Options;
+
 using Nerve.Dns.Client;
 using Nerve.Dns.Resolver;
+using Nerve.Dns.Resolver.Blocklist;
 using Nerve.Dns.Server;
 
 namespace Nerve.Service.Extensions;
@@ -15,11 +18,19 @@ public static class NerveServiceCollectionExtensions
     public static IServiceCollection AddNerve(this IServiceCollection @this, IConfiguration configuration)
     {
         @this.Configure<NerveOptions>(configuration.GetSection("Nerve"));
+
+        @this.AddSingleton<IDomainBlocklistService, DomainBlocklistService>();
         @this.AddSingleton<IDnsServer>(
             serviceProvider =>
             {
-                var resolver = new DnsClientResolver(new UdpDnsClient(IPAddress.Parse("1.1.1.1")));
-                return new UdpDnsServer(serviceProvider.GetRequiredService<ILogger<UdpDnsServer>>(), new IPEndPoint(IPAddress.Parse("0.0.0.0"), 5333), resolver);
+                var nerveOptions = serviceProvider.GetRequiredService<IOptions<NerveOptions>>();
+
+                IIpEndPointProvider ipEndPointProvider = nerveOptions.Value.Forwarders.Length == 1
+                    ? new SingleIpEndPointProvider(new IPEndPoint(IPAddress.Parse(nerveOptions.Value.Forwarders[0]), 53))
+                    : new RoundRobinIpEndPointProvider(nerveOptions.Value.Forwarders.Select(ip => new IPEndPoint(IPAddress.Parse(ip), 53)).ToArray());
+                var dnsClientResolver = new DnsClientResolver(new UdpDnsClient(ipEndPointProvider));
+                var resolver = new DomainBlocklistResolver(serviceProvider.GetRequiredService<IDomainBlocklistService>(), dnsClientResolver);
+                return new UdpDnsServer(serviceProvider.GetRequiredService<ILogger<UdpDnsServer>>(), new IPEndPoint(IPAddress.Parse(nerveOptions.Value.Ip), nerveOptions.Value.Port), resolver);
             });
 
         @this.AddHostedService<NerveBackgroundService>();
