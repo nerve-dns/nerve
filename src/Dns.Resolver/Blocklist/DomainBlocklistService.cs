c2370d5b1a385f1447133d52bc88e31507097346
varelen <varelen@proton.me>
2023-08-24 21:37:15 +0200

    feat(Dns.Resolver): Make domain blocklist service thread safe

--- src/Dns.Resolver/Blocklist/DomainBlocklistService.cs
+++ src/Dns.Resolver/Blocklist/DomainBlocklistService.cs
@@ -10,80 +10,138 @@ public sealed class DomainBlocklistService : IDomainBlocklistService
 {
     private readonly Dictionary<IPAddress, CompiledBlocklist> blocklist = new();
     private long size = 0;
+    private ReaderWriterLockSlim readerWriterLockSlim = new();
 
     public long Size => this.size;
 
     public void Add(IPAddress remoteIp, string domain, string ip)
     {
-        this.size += 1;
+        this.readerWriterLockSlim.EnterWriteLock();
 
-        if (this.blocklist.TryGetValue(remoteIp, out var compiledBlocklist))
+        try
         {
-            compiledBlocklist.Add(domain, ip);
-            return;
+            this.size += 1;
+
+            if (this.blocklist.TryGetValue(remoteIp, out var compiledBlocklist))
+            {
+                compiledBlocklist.Add(domain, ip);
+                return;
+            }
+
+            var newCompiledBlocklist = new CompiledBlocklist(new Dictionary<string, string>()
+            {
+                { domain, ip },
+            });
+            this.blocklist.Add(remoteIp, newCompiledBlocklist);
         }
-
-        var newCompiledBlocklist = new CompiledBlocklist(new Dictionary<string, string>()
+        finally
         {
-            { domain, ip },
-        });
-        this.blocklist.Add(remoteIp, newCompiledBlocklist);
+            this.readerWriterLockSlim.ExitWriteLock();
+        }
     }
 
     public void Add(IPAddress remoteIp, Dictionary<string, string> domainsAndIps)
     {
-        this.size += domainsAndIps.Count;
+        this.readerWriterLockSlim.EnterWriteLock();
 
-        if (this.blocklist.TryGetValue(remoteIp, out var compiledBlocklist))
+        try
         {
-            compiledBlocklist.Add(domainsAndIps);
-            return;
-        }
+            this.size += domainsAndIps.Count;
 
-        var newCompiledBlocklist = new CompiledBlocklist(domainsAndIps);
-        this.blocklist.Add(remoteIp, newCompiledBlocklist);
+            if (this.blocklist.TryGetValue(remoteIp, out var compiledBlocklist))
+            {
+                compiledBlocklist.Add(domainsAndIps);
+                return;
+            }
+
+            var newCompiledBlocklist = new CompiledBlocklist(domainsAndIps);
+            this.blocklist.Add(remoteIp, newCompiledBlocklist);
+        }
+        finally
+        {
+            this.readerWriterLockSlim.ExitWriteLock();
+        }
     }
 
     public bool Remove(IPAddress remoteIp, string domain)
     {
-        this.size -= 1;
-
-        if (this.blocklist.TryGetValue(remoteIp, out var compiledBlocklist))
+        this.readerWriterLockSlim.EnterWriteLock();
+        
+        try
         {
-            return compiledBlocklist.Remove(domain);
-        }
+            this.size -= 1;
 
-        return false;
+            if (this.blocklist.TryGetValue(remoteIp, out var compiledBlocklist))
+            {
+                return compiledBlocklist.Remove(domain);
+            }
+
+            return false;
+        }
+        finally
+        {
+            this.readerWriterLockSlim.ExitWriteLock();
+        }
     }
 
     public bool Remove(IPAddress remoteIp)
     {
-        if (this.blocklist.TryGetValue(remoteIp, out CompiledBlocklist? compiledBlocklist))
+        this.readerWriterLockSlim.EnterWriteLock();
+
+        try
         {
-            this.size -= compiledBlocklist.Blocklist.Count;
-            return this.blocklist.Remove(remoteIp);
+            if (this.blocklist.TryGetValue(remoteIp, out CompiledBlocklist? compiledBlocklist))
+            {
+                this.size -= compiledBlocklist.Blocklist.Count;
+                return this.blocklist.Remove(remoteIp);
+            }
+
+            return false;
         }
-        return false;
+        finally
+        {
+            this.readerWriterLockSlim.ExitWriteLock();
+        }
     }
 
     public bool TryGet(IPAddress remoteIp, out CompiledBlocklist? compiledBlocklist)
-        => this.blocklist.TryGetValue(remoteIp, out compiledBlocklist);
+    {
+        this.readerWriterLockSlim.EnterReadLock();
+        
+        try
+        {
+            return this.blocklist.TryGetValue(remoteIp, out compiledBlocklist);
+        }
+        finally
+        {
+            this.readerWriterLockSlim.ExitReadLock();
+        }
+    }
 
     public bool IsBlocked(IPAddress remoteIp, string domain, out string? ip)
     {
-        // Global blocklist
-        if (this.blocklist.TryGetValue(IPAddress.Any, out var globalCompiledBlocklist))
-        {
-            return globalCompiledBlocklist.IsBlocked(domain, out ip);
-        }
+        this.readerWriterLockSlim.EnterReadLock();
 
-        // IP specific blocklist
-        if (this.blocklist.TryGetValue(remoteIp, out var compiledBlocklist))
+        try
         {
-            return compiledBlocklist.IsBlocked(domain, out ip);
-        }
+            // Global blocklist
+            if (this.blocklist.TryGetValue(IPAddress.Any, out var globalCompiledBlocklist))
+            {
+                return globalCompiledBlocklist.IsBlocked(domain, out ip);
+            }
 
-        ip = null;
-        return false;
+            // IP specific blocklist
+            if (this.blocklist.TryGetValue(remoteIp, out var compiledBlocklist))
+            {
+                return compiledBlocklist.IsBlocked(domain, out ip);
+            }
+
+            ip = null;
+            return false;
+        }
+        finally
+        {
+            this.readerWriterLockSlim.ExitReadLock();
+        }
     }
 }
