8674356b887cd2284c2e3c9ba574a899c71b8556
varelen <varelen@proton.me>
2023-08-26 13:11:15 +0200

    refact(Dns.Client): Correctly resolve remote DoH host and use it to query it

--- src/Dns.Client/HttpsDnsClient.cs
+++ src/Dns.Client/HttpsDnsClient.cs
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 using System.Buffers;
+using System.Net;
 using System.Net.Http.Headers;
 
 namespace Nerve.Dns.Client;
@@ -12,16 +13,22 @@ public sealed class HttpsDnsClient : IDnsClient
     private const string DnsMessageContentType = "application/dns-message";
     private const int MaxUdpDatagramSize = 512;
 
+    private readonly UdpDnsClient udpDnsClient;
     private readonly IUriProvider uriProvider;
     private readonly HttpClient httpClient;
     private readonly Random random;
+    // TODO: Clear this cache after X minutes to not have stale resolved IP addresses?
+    private readonly Dictionary<Uri, Uri> resolvedUrisCache;
 
     public HttpsDnsClient(IUriProvider uriProvider)
     {
+        // TODO: What to do with this hardcoded resolver IP?
+        this.udpDnsClient = new UdpDnsClient(IPAddress.Parse("1.1.1.1"));
         this.uriProvider = uriProvider;
         this.httpClient = new HttpClient();
         this.httpClient.DefaultRequestHeaders.Add("Accept", DnsMessageContentType);
         this.random = new Random();
+        this.resolvedUrisCache = new Dictionary<Uri, Uri>();
     }
 
     public HttpsDnsClient(Uri uri)
@@ -74,7 +81,23 @@ public sealed class HttpsDnsClient : IDnsClient
             var byteArrayContent = new ByteArrayContent(bytes, 0, offset);
             byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(DnsMessageContentType);
 
-            HttpResponseMessage httpResponseMessage = await this.httpClient.PostAsync(this.uriProvider.Get(), byteArrayContent, cancellationToken);
+            Uri originalUri = this.uriProvider.Get();
+            if (!this.resolvedUrisCache.TryGetValue(originalUri, out Uri? resolvedUri))
+            {
+                resolvedUri = await this.ResolveUriAsync(originalUri);
+                this.resolvedUrisCache.Add(originalUri, resolvedUri);
+            }
+
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = resolvedUri,
+                Content = byteArrayContent,
+                Headers = {
+                    { "Host", originalUri.Host }
+                },
+            };
+            HttpResponseMessage httpResponseMessage = await this.httpClient.SendAsync(httpRequestMessage, cancellationToken);
             httpResponseMessage.EnsureSuccessStatusCode();
 
             byte[] responseMessageBytes = await httpResponseMessage.Content.ReadAsByteArrayAsync(cancellationToken);
@@ -88,5 +111,15 @@ public sealed class HttpsDnsClient : IDnsClient
         {
             ArrayPool<byte>.Shared.Return(bytes);
         }
+    }
+
+    // TODO: Is this the right/best way? You have to somehow resolve the DoH server endpoint somehow..
+    private async Task<Uri> ResolveUriAsync(Uri uri)
+    {
+        var uriBuilder = new UriBuilder(uri);
+        // TODO: Only IPv4 (A) supported, what about IPv6 (AAAA)?
+        Message message = await this.udpDnsClient.ResolveAsync(new Question(new DomainName(uri.Host), Type.A, Class.In));
+        uriBuilder.Host = ((AResourceData)message.Answers.First().ResourceData!).Address.ToString();
+        return uriBuilder.Uri;
     }
 }
