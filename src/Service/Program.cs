3e862a5122dfd7de5f8b962debafa42e42eee84f
varelen <varelen@proton.me>
2023-08-29 19:37:02 +0200

    feat(Service): Add ability to restart the app in process

--- src/Service/Program.cs
+++ src/Service/Program.cs
@@ -8,42 +8,82 @@ using Nerve.Service.Domain;
 using Nerve.Service.Extensions;
 using Nerve.Service.Web;
 
-IConfiguration configuration = new ConfigurationBuilder()
-    .AddJsonFile("nerve.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables()
-    .AddCommandLine(args)
-    .Build();
+namespace Nerve.Service;
 
-using IHost host = new HostBuilder()
-    .ConfigureAppConfiguration(builder =>
-    {
-        builder.AddConfiguration(configuration);
-    })
-    .ConfigureServices((hostContext, services) =>
-    {
-        services.AddNerve(configuration);
-    })
-    .ConfigureWebHost(webConfig =>
-    {
-        webConfig.UseKestrel();
-        webConfig.UseStartup<NerveWebStartup>();
-        webConfig.UseUrls("http://0.0.0.0/");
-        webConfig.ConfigureLogging(logging =>
-        {
-            logging
-                .AddConfiguration(configuration.GetSection("Logging"))
-                .AddConsole();
-        });
-    })
-    .Build();
-
-using (IServiceScope serviceScope = host.Services.CreateScope())
+public static class Program
 {
-    var webHostEnvironment = serviceScope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+    private static CancellationTokenSource cancellationTokenSource = new();
+    private static bool keepRestarting;
 
-    var db = serviceScope.ServiceProvider.GetRequiredService<NerveDbContext>();
+    private static async Task Main(string[] args)
+    {
+        await StartAsync(args);
 
-    db.Database.Migrate();
+        while (keepRestarting)
+        {
+            Console.WriteLine("Restarting Nerve");
+
+            keepRestarting = false;
+            await StartAsync(args);
+        }
+    }
+
+    public static void Restart()
+    {
+        keepRestarting = true;
+        cancellationTokenSource.Cancel();
+    }
+
+    private static async Task StartAsync(string[] args)
+    {
+        try
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("nerve.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            using IHost host = new HostBuilder()
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddConfiguration(configuration);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddNerve(configuration);
+                })
+                .ConfigureWebHost(webConfig =>
+                {
+                    webConfig.UseKestrel();
+                    webConfig.UseStartup<NerveWebStartup>();
+                    webConfig.UseUrls("http://0.0.0.0:50000/");
+                    webConfig.UseWebRoot(Path.Combine(Directory.GetCurrentDirectory(), "management"));
+                    webConfig.ConfigureLogging(logging =>
+                    {
+                        logging
+                            .AddConfiguration(configuration.GetSection("Logging"))
+                            .AddConsole();
+                    });
+                })
+                .Build();
+
+            using (IServiceScope serviceScope = host.Services.CreateScope())
+            {
+                var webHostEnvironment = serviceScope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+
+                var db = serviceScope.ServiceProvider.GetRequiredService<NerveDbContext>();
+
+                db.Database.Migrate();
+            }
+
+            await host.RunAsync(cancellationTokenSource.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            // Ignore
+        }
+    }
 }
-
-await host.RunAsync();
